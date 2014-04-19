a28a27a35f91e18119f9ef76c89dedd56fe2b4bd
chyxwzn <chyxwzn@gmail.com>
2014-04-19 12:38:49 +0800

    git configure

---
+++ gitconf.sh
@@ -0,0 +1,12 @@
+git config --global http.proxy 127.0.0.1:8087
+git config --global user.name "chyxwzn"
+git config --global user.email chyxwzn@gmail.com
+git config --global merge.tool vimdiff
+git config --global credential.helper cache --timeout=36000
+git config --global diff.tool vimdiff
+git config --global difftool.prompt false
+git config --global difftool.prompt false
+git config --global alias.d difftool
+git config --global color.diff always
+git config --global color.grep=always
+git config --global grep.linenumber true
