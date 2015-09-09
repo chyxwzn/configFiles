e9100b2fb561675b4e85fb6e2eb1ece697d8a4a7
chyxwzn <chyxwzn@gmail.com>
2015-09-10 00:43:42 +0800

    Update gitconf.sh

--- gitconf.sh
+++ gitconf.sh
@@ -6,8 +6,14 @@ git config --global credential.helper cache --timeout=36000
 git config --global diff.tool vimdiff
 git config --global difftool.prompt false
 git config --global difftool.prompt false
-git config --global alias.d difftool
 git config --global color.diff always
 git config --global color.grep always
 git config --global grep.linenumber true
 git config --global http.sslVerify false
+git config --global alias.d difftool
+git config --global alias.st status
+git config --global alias.co checkout
+git config --global alias.ci commit
+git config --global alias.br branch
+git config --global alias.unstage 'reset HEAD'
+git config --global alias.lg "log --color --graph --pretty=format:'%Cred%h%Creset -%C(yellow)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --abbrev-commit"
